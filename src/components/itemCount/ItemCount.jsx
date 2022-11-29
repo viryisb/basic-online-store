56d37c73ffa00b093993660c30ad52c218aa259f
Virginia <vbernasconi@altimetrik.com>
2022-11-28 22:23:08 -0300

    Pass handlers as callback to buttons

--- src/components/itemCount/ItemCount.jsx
+++ src/components/itemCount/ItemCount.jsx
@@ -1,29 +1,28 @@
 import { React, useState } from 'react';
 import './ItemCount.css';
 
-const ItemCount = () => {
-  const [sum, setSum] = useState(0);
-  const [add, setAdd] = useState(0);
+const ItemCount = ({ stock, initial, onAdd }) => {
+  const [sum, setSum] = useState(initial);
 
-  const stock = 10;
   const buttonClickSumHandler = () => {
     return sum < stock ? setSum(sum + 1) : stock;
   };
   const buttonClickRestHandler = () => {
     return sum > 0 ? setSum(sum - 1) : 0;
   };
-  const onAdd = () => {
-    setAdd(sum);
-  };
+
   return (
     <>
       <div className='cart-btn-container'>
-        <button className='cart-btn' onClick={onAdd}>
+        <button className='cart-btn' onClick={() => onAdd(sum)}>
           <span>Add to cart</span>
         </button>
-        <span>{add}</span>
+
         <div className='input-group input-number-group'>
-          <div className='input-group-button' onClick={buttonClickRestHandler}>
+          <div
+            className='input-group-button'
+            onClick={() => buttonClickRestHandler()}
+          >
             <span className='input-number-decrement'>-</span>
           </div>
           <input
@@ -35,7 +34,7 @@ const ItemCount = () => {
           ></input>
           <button
             className='input-group-button'
-            onClick={buttonClickSumHandler}
+            onClick={() => buttonClickSumHandler()}
           >
             <span className='input-number-increment'>+</span>
           </button>
