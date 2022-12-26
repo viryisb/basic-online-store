4598fcb69a93528164703c0358e91d532530abcd
Virginia <vbernasconi@altimetrik.com>
2022-12-26 17:21:13 -0300

    Add cartContext component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ItemDetailContainer from './components/itemDetailContainer/ItemDetailContainer';
-
+import { CartContext } from './context/CartContext';
+import React, { useContext } from 'react';
 import ItemListContainer from './components/itemListContainer/ItemListContainer';
 import NavBar from './components/navBar/NavBar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
+    <CartContext.Provider value={[]}>
+      <BrowserRouter>
+        <NavBar />
 
-      <Routes>
-        <Route exact path='/' element={<ItemListContainer />} />
-        <Route
-          exact
-          path='category/:categoryId'
-          element={<ItemListContainer />}
-        />
-        <Route exact path='/item/:id' element={<ItemDetailContainer />} />
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<ItemListContainer />} />
+          <Route
+            exact
+            path='category/:categoryId'
+            element={<ItemListContainer />}
+          />
+          <Route exact path='/item/:id' element={<ItemDetailContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </CartContext.Provider>
   );
 }
 
