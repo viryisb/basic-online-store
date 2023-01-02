4d2267141334efb2424a7d48f5ee9e05fd213fe4
Virginia <vbernasconi@altimetrik.com>
2023-01-02 20:16:26 -0300

    implementing firestore

--- src/components/itemDetailContainer/ItemDetailContainer.js
+++ src/components/itemDetailContainer/ItemDetailContainer.js
@@ -1,7 +1,6 @@
 import { React, useEffect, useState } from 'react';
 import ItemDetail from '../itemDetail/ItemDetail';
-
-import items from '../../mockedData';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import { useParams } from 'react-router-dom';
 import Loader from '../Loader/Loader';
 
@@ -10,19 +9,30 @@ const ItemDetailContainer = () => {
   const [loading, setLoading] = useState(false);
   const { id } = useParams();
 
+  console.log(id);
+
   useEffect(() => {
     setLoading(true);
-    const getDetail = new Promise((resolve) => {
-      setTimeout(() => {
-        setLoading(false);
-        resolve(items);
-      }, 2000);
+    const db = getFirestore();
+    const getDetail = doc(db, 'items', id);
+    getDoc(getDetail).then((snapshot) => {
+      setLoading(false);
+      if (snapshot.exists()) {
+        setProduct({ id: snapshot.id, ...snapshot.data() });
+      }
     });
-    getDetail.then((res) =>
-      setProduct(res.find((prod) => prod.id === parseInt(id)))
-    );
   }, [id]);
 
   return <>{loading ? <Loader /> : <ItemDetail item={product} />}</>;
 };
 export default ItemDetailContainer;
+/* useEffect(() => {
+    setLoading(true);
+    const getDetail = new Promise((resolve) => {
+      setLoading(false);
+      resolve(items);
+    });
+    getDetail.then((res) =>
+      setProduct(res.find((prod) => prod.id === parseInt(id)))
+    );
+  }, [id]); */
